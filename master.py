682479fbeebe17a471861bd091aae458f9d36bcc
Sumit Satym <sumitsatyam2611@gmail.com>
2023-07-24 19:51:42 +0530

    Testing of stash in feature-branch2

--- master.py
+++ master.py
@@ -16,7 +16,7 @@ wrld_df = spark.read.csv("C:/file/world_population.csv", inferSchema=True, heade
 # wrld_df.show()
 # wrld_df.filter((wrld_df.Country != 'India') & (wrld_df.Continent == 'Asia')).show(truncate=False)
 wrld_df.select('Country', 'Continent').show(truncate=False)
-# write.csv("C:/Users/Desktop/pyspark/output/output.csv")
+# write.csv("C:/Users/abc/Desktop/pyspark/output/output.csv")
 
 # show(truncate=True)
 #print("added from editor")
